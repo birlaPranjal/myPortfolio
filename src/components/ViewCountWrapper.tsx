ef02d928e44de0c5b5ade56493a4e356e1d25d91
pranjal <birlapranjal460@gmail.com>
2024-06-30 18:10:14 +0530

    Fix 'self not defined' error

--- src/components/ViewCountWrapper.tsx
+++ src/components/ViewCountWrapper.tsx
@@ -2,9 +2,6 @@
 "use client";
 
 import dynamic from 'next/dynamic';
-
 const ViewCount = dynamic(() => import('./ViewCount'), { ssr: false });
-
 const ViewCountWrapper = () => <ViewCount />;
-
 export default ViewCountWrapper;
