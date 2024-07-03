ea23fbef33c4a9385e58f285cff504625ed9956d
Pranjal birla <birlapranjal460@gmail.com>
2024-07-03 22:11:37 +0530

    centered the view count and svg

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,41 +1,41 @@
 "use client";
 import React, { useState } from "react";
-import navLinks from "../../public/data/link"
-import  {useRouter}  from "next/navigation";
+import navLinks from "../../public/data/link";
+import { useRouter } from "next/navigation";
 import ViewCountWrapper from "./ViewCountWrapper";
-
 
 const Header: React.FC = () => {
   const [isOpen, setIsOpen] = useState(false);
-  const [count, setCount] = useState<number>(0);
   const router = useRouter();
 
   const toggleMenu = () => {
     setIsOpen(!isOpen);
   };
 
-  const handleScroll = async (e: React.MouseEvent<HTMLAnchorElement, MouseEvent>, path: string) => {
+  const handleScroll = async (
+    e: React.MouseEvent<HTMLAnchorElement, MouseEvent>,
+    path: string
+  ) => {
     e.preventDefault();
-    if (path.startsWith('/#')) {
+    if (path.startsWith("/#")) {
       const targetId = path.substring(2);
       const targetElement = document.getElementById(targetId);
       if (targetElement) {
-        targetElement.scrollIntoView({ behavior: 'smooth' });
+        targetElement.scrollIntoView({ behavior: "smooth" });
       }
     } else {
       await router.push(path);
-      if (path.includes('#')) {
-        const targetId = path.split('#')[1];
+      if (path.includes("#")) {
+        const targetId = path.split("#")[1];
         setTimeout(() => {
           const targetElement = document.getElementById(targetId);
           if (targetElement) {
-            targetElement.scrollIntoView({ behavior: 'smooth' });
+            targetElement.scrollIntoView({ behavior: "smooth" });
           }
         }, 100);
       }
     }
   };
-
 
   return (
     <header className="py-4 shadow-md h-[3.8rem] top-0 bg-transparent z-50 font-mono">
@@ -51,16 +51,16 @@ const Header: React.FC = () => {
           ))}
         </nav>
 
-        <div className="sm:hidden">
+        <div className="sm:hidden ">
           <button onClick={toggleMenu}>
             {!isOpen ? (
-              <div className='flex'>
+              <div className="flex items-center justify-center">
               <div className="block sm:hidden">
-          <div className="flex">
-            <ViewCountWrapper/>
-          </div>
-        </div>
-                <svg
+                <div className="flex mt-2">
+                  <ViewCountWrapper />
+                </div>
+              </div>
+              <svg
                 xmlns="http://www.w3.org/2000/svg"
                 x="0px"
                 y="0px"
@@ -72,9 +72,15 @@ const Header: React.FC = () => {
               >
                 <path d="M 3 9 A 1.0001 1.0001 0 1 0 3 11 L 47 11 A 1.0001 1.0001 0 1 0 47 9 L 3 9 z M 3 24 A 1.0001 1.0001 0 1 0 3 26 L 47 26 A 1.0001 1.0001 0 1 0 47 24 L 3 24 z M 3 39 A 1.0001 1.0001 0 1 0 3 41 L 47 41 A 1.0001 1.0001 0 1 0 47 39 L 3 39 z"></path>
               </svg>
-              </div>
-              
+            </div>
             ) : (
+              
+              <div className="flex items-center justify-center">
+              <div className="block sm:hidden">
+                <div className="flex mt-2">
+                  <ViewCountWrapper />
+                </div>
+              </div>
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 x="0px"
@@ -87,6 +93,7 @@ const Header: React.FC = () => {
               >
                 <path d="M 7.71875 6.28125 L 6.28125 7.71875 L 23.5625 25 L 6.28125 42.28125 L 7.71875 43.71875 L 25 26.4375 L 42.28125 43.71875 L 43.71875 42.28125 L 26.4375 25 L 43.71875 7.71875 L 42.28125 6.28125 L 25 23.5625 Z"></path>
               </svg>
+            </div>
             )}
           </button>
 
@@ -95,7 +102,10 @@ const Header: React.FC = () => {
               <nav className="flex flex-col justify-center items-center space-y-4 px-4 pt-6 pb-8">
                 {navLinks?.map((link) => (
                   <li className="list-none z-50" key={link.title}>
-                    <a href={link.path} onClick={(e) => handleScroll(e, link.path)}>
+                    <a
+                      href={link.path}
+                      onClick={(e) => handleScroll(e, link.path)}
+                    >
                       {link.title}
                     </a>
                     <hr className="w-full border-gray-700" />
@@ -107,7 +117,7 @@ const Header: React.FC = () => {
         </div>
         <div className="hidden sm:block">
           <div className="flex">
-            <ViewCountWrapper/>
+            <ViewCountWrapper />
           </div>
         </div>
       </div>
